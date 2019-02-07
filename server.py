8666378d7d4bb5ee35fac6fcf1636e942a980a8b
Anish Basu <anish@drexel.edu>
2019-02-06 19:33:59 -0500

    Added server skeleton.

---
+++ server.py
@@ -0,0 +1,14 @@
+from net.TaskService_pb2 import TaskService
+
+class TaskServer(TaskService):
+    def AddTask(self, request, context):
+        pass
+    
+    def AddArgument(self, request, context):
+        pass
+    
+    def ExecTask(self, request, context):
+        pass
+
+    def PollTask(self, request, context):
+        pass
