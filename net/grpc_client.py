f4efcb275868e8e77c96d8c88de5b08a05933d3d
Anish Basu <anish@drexel.edu>
2019-03-06 22:50:48 -0500

    Added Arg dumping and chunking

--- net/grpc_client.py
+++ net/grpc_client.py
@@ -1,7 +1,8 @@
 import grpc
+import dill as pickle
 import net.proto.TaskService_pb2_grpc as TaskService
-from net.proto.TaskMetadata_pb2 import TaskMetadata, TaskArgument
-from net.proto.TaskCommon_pb2 import StatusCode
+from net.proto.TaskMetadata_pb2 import TaskMetadata, TaskArgument, ArgumentMetadata, ARGUMENT, KEYWORD_ARGUMENT
+from net.proto.TaskCommon_pb2 import OK
 
 class TaskClient:
     def __init__(self, host, port, cert_file = None):
@@ -9,23 +10,33 @@ class TaskClient:
         self.__client = TaskService.TaskServiceStub(self.__channel)
 
     def send_promise(self, promise):
-        _task = TaskMetadata(module   = promise.__module,
-                             function = promise.__function,
-                             args     = len(promise.__args),
-                             kwargs   = promise.__kwargs.keys(),
-                             _class   = promise.__class)
-
+        _task = promise.__meta__()
         #sends the task metadata, resp is the TaskReceived
         resp = self.__client.AddTask(_task)
-        if resp.status.code == StatusCode.OK:
+        if resp.status.code == OK:
             promise.__task_id = resp.task_id
+            for arg in promise.__args:
+                # C H O N K
+                self.__client
         else:
             #add error class
             pass
 
-    def make_argument(self, ord, arg):
-        _arg = TaskArgument()
+    def make_argument(self, task_id, ord, arg):
+        arg_seq = ArgumentMetadata(task_id = task_id)
         if isinstance(ord, str):
+            arg_seq.arg_type = KEYWORD_ARGUMENT
+            arg_seq.name = ord
+        else:
+            arg_seq.arg_type = ARGUMENT
+            arg_seq.num = ord
+        yield TaskArgument(arg_seq = arg_seq)
+
+        bin_arg = pickle.dumps(arg)
+        mv_arg = memoryview(bin_arg)
+        chunks = len(bin_arg) / (1 << 20)
+        for i in range(chunks):
+            yield TaskArgument(arg = mv_arg[i * chunks : (i + 1) * chunks])
 
     def exec_promise(self, Promise):
         pass
