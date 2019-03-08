2247cd736586012cd75ddff7c4f92182a69afaee
Anish Basu <anish@drexel.edu>
2019-03-08 18:00:13 -0500

    Add Promise functions

--- Promise.py
+++ Promise.py
@@ -1,4 +1,5 @@
-from net.proto.TaskMetadata_pb2 import TaskMetadata
+from net.proto.TaskMetadata_pb2 import TaskMetadata, ExecTaskRequest
+from net.grpc_client import TaskClientInstance
 class Promise:
     def __init__(self, module, function, args, kwargs, _class=None):
         self.__module = module
@@ -14,7 +15,20 @@ class Promise:
                             args     = len(self.__args),
                             kwargs   = self.__kwargs.keys(),
                             _class   = self.__class)
+    def set_task_id(self, task_id):
+        self.__task_id = task_id
+    def task_id(self):
+        return self.__task_id
 
-    def exec(self):
-        #Halt on pending results
-        pass
+    def send_args(self, client):
+        if len(self.__args) > 0:
+            for idx, arg in enumerate(self.__args):
+                client.AddArgument.future(client.make_argument(self.__task_id, idx, arg))
+    def send_kwargs(self, client):
+        if len(self.__kwargs.keys()) > 0:
+            for key, arg in self.__kwargs.items():
+                self.__client.AddArgument.future(client.make_argument(self.__task_id, key, arg))
+
+
+    def exec(self, client):
+        return client.exec_promise(self)
