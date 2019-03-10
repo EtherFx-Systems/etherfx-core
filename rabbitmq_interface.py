314389d98ef1bf224ea0f23e0373adf0bdb0b115
Mridul <mridul.singhai@instacart.com>
2019-03-10 16:06:30 -0400

    Configurable callback

--- rabbitmq_interface.py
+++ rabbitmq_interface.py
@@ -17,13 +17,11 @@ class RabbitMQInterface:
     def publishToQueue(self):
         connection = self.create_connection(IP_ADDRESS)
         channel = connection.channel()
-
         channel.queue_declare(queue="demo_publishing_queue")
-
         channel.basic_publish(
             exchange="", routing_key="demo_publishing_queue", body=self.payload
         )
-        print(" Sent Message from Orchestrator")
+        print("Sent Message from Orchestrator")
         connection.close()
 
     def publish_taskMetadata_to_queue_from_orchestrator(self, TaskMetaData):
@@ -34,7 +32,7 @@ class RabbitMQInterface:
         self.payload = pickle.dumps(TaskMetaData)
         self.publishToQueue()
 
-    def subscribe_from_queue(self):
+    def subscribe_to_queue(self, callback_method=self.callback):
         connection = self.create_connection(IP_ADDRESS)
         channel = connection.channel()
         channel.queue_declare(queue="demo_publishing_queue")
@@ -47,8 +45,7 @@ class RabbitMQInterface:
             auto_delete=False,
         )
 
-        channel.basic_consume(self.callback, queue="demo_publishing_queue", no_ack=True)
-
+        channel.basic_consume(callback_method, queue="demo_publishing_queue", no_ack=True)
         channel.start_consuming()
 
     def callback(self, ch, method, properties, body):
